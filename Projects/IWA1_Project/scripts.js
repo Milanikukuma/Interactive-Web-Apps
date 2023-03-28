203f61b65ac6fc25dbe9edfab13475d5240e44e0
Milanikukuma <Kukumamilani62@gmail>
2023-03-28 14:37:22 +0200

    updating the functionality of the code

--- Projects/IWA1_Project/scripts.js
+++ Projects/IWA1_Project/scripts.js
@@ -1,15 +1,15 @@
-const Max_NUMBER = 15
+const MAX_NUMBER = 15
 const MIN_NUMBER = -5
 
-const number = document.querySelector('{data-key="number"}')
-const subtract =document.querySelector('{data-key="subtract"}')
-const add = document.querySelector('{data-key="add"}')
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
 
-const substractHandler = () =>{
- const newValue = parseInt(number.value) -1
+const subtractHandler = () =>{
+ const newValue = parseInt(number.value) -1;
  number.value = newValue 
 
-   if (add.disabled === true) {
+   if (number.disabled === true) {
        add.disabled = false
    }
 
@@ -19,20 +19,19 @@ const substractHandler = () =>{
 }
 
 const addHandler = () => {
-const newValue = parseInt(number.value) +1  
-number.value = newValue
+    const newValue = parseInt(number.value) + 1;
+    number.value = newValue;
 
-if (subtract.disabled === true) {
-    subtract.disabled = false
-   }
+    if (number.value <= MIN_NUMBER){
+        add.disabled = false;
+    } 
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true;
 
-   if (newValue >= MIN_NUMBER) {
-      add.disabled = true
-   }
-
-
+    }
 }
 
 
 subtract.addEventListener('click', subtractHandler)
-add.addEventListeners('click',addHandler)
+
+add.addEventListener('click', addHandler)
