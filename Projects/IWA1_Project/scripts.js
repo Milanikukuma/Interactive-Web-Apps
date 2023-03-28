0ab5d06392a1cf736f8fe5d96af0eabc8b1d7125
Milanikukuma <Kukumamilani62@gmail>
2023-03-28 02:31:01 +0200

    new changes

---
+++ Projects/IWA1_Project/scripts.js
@@ -0,0 +1,38 @@
+const Max_NUMBER = 15
+const MIN_NUMBER = -5
+
+const number = document.querySelector('{data-key="number"}')
+const subtract =document.querySelector('{data-key="subtract"}')
+const add = document.querySelector('{data-key="add"}')
+
+const substractHandler = () =>{
+ const newValue = parseInt(number.value) -1
+ number.value = newValue 
+
+   if (add.disabled === true) {
+       add.disabled = false
+   }
+
+   if (newValue <= MIN_NUMBER) {
+       subtract.disabled = true
+   }
+}
+
+const addHandler = () => {
+const newValue = parseInt(number.value) +1  
+number.value = newValue
+
+if (subtract.disabled === true) {
+    subtract.disabled = false
+   }
+
+   if (newValue >= MIN_NUMBER) {
+      add.disabled = true
+   }
+
+
+}
+
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListeners('click',addHandler)
