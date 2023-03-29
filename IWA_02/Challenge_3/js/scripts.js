8aa7c26b3a35a8b008b2a7763eca58493f76005c
Milanikukuma <Kukumamilani62@gmail>
2023-03-29 22:45:12 +0200

    uploading challenge 1-3

---
+++ IWA_02/Challenge_3/js/scripts.js
@@ -0,0 +1,8 @@
+// Print a warning message to the console to indicate that a security scan is starting.
+console.warn('Security scan starting');
+
+/* 
+ * It is important to let the user know when they can safely close the page. 
+ * This message informs them to wait for the scan to complete before closing the browser.
+ */
+console.info('Please wait for the scan to complete before closing the browser.');
